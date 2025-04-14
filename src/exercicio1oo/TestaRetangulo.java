cc88e4bf078262775747588b99c3dc6bfc1f21bc
Herculano De Biasi <02105018980@udesc.br>
2025-04-14 20:19:24 -0300

    Classe retangulo e respectivo teteste

---
+++ src/exercicio1oo/TestaRetangulo.java
@@ -0,0 +1,11 @@
+package exercicio1oo;
+
+class TestaRetangulo {
+    public static void main(String[] args) {
+        Retangulo r = new Retangulo();
+        r.largura = 3.0;
+        r.altura = 4.0;
+        System.out.println("Largura: " + r.largura);
+        System.out.println("Altura: " + r.altura);
+    }
+}
